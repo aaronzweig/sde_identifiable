086fb7dc3a8567f9c953b97d5f1e21efbeb3b9dd
aaronzweig <az831@nyu.edu>
2025-09-22 16:50:15 -0400

    updated req and bash

---
+++ new_experiments/bash_linear_sweep.sh
@@ -0,0 +1,4 @@
+python sde_linear_script.py --n 100 --fix_decay True
+python sde_linear_script.py --n 200 --fix_decay True
+python sde_linear_script.py --n 100 --fix_decay False
+python sde_linear_script.py --n 200 --fix_decay False
